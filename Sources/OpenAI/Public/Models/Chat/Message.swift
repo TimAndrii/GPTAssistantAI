c99154f27d6cc3680450563b189c3855036ca33f
Andrii Tim <and.tim@icloud.com>
2024-02-18 17:45:03 +0200

    add data to message

--- Sources/OpenAI/Public/Models/Chat/Message.swift
+++ Sources/OpenAI/Public/Models/Chat/Message.swift
@@ -16,7 +16,9 @@ public struct Message: Codable, Equatable {
     
     /// The name of the author of this message. `name` is required if role is `function`, and it should be the name of the function whose response is in the `content`. May contain a-z, A-Z, 0-9, and underscores, with a maximum length of 64 characters.
     public let name: String?
-    
+
+    public let imageData: Data?
+
     @available(*, deprecated, message: "use toolCalls instead")
     public let functionCall: ChatFunctionCall?
     
@@ -34,10 +36,11 @@ public struct Message: Codable, Equatable {
         case content
         case name
         case functionCall = "function_call"
+        case imageData
 //        case toolCalls = "tool_calls"
     }
     
-    public init(role: Role, content codable: StringOrChatContent? = nil, name: String? = nil, toolCalls: [ToolCall]? = nil) {
+    public init(role: Role, content codable: StringOrChatContent? = nil, name: String? = nil, toolCalls: [ToolCall]? = nil, imageData: Data? = nil) {
         let stringOrCodable: StringOrCodable<[ChatContent]>?
         
         if let string = codable as? String {
@@ -48,10 +51,10 @@ public struct Message: Codable, Equatable {
             stringOrCodable = nil
         }
         
-        self.init(role: role, content: stringOrCodable, name: name, toolCalls: toolCalls)
+        self.init(role: role, content: stringOrCodable, name: name, toolCalls: toolCalls, imageData: imageData)
     }
     
-    public init(role: Role, content codable: StringOrChatContent? = nil, name: String? = nil, functionCall: ChatFunctionCall? = nil) {
+    public init(role: Role, content codable: StringOrChatContent? = nil, name: String? = nil, functionCall: ChatFunctionCall? = nil, imageData: String? = nil) {
         let stringOrCodable: StringOrCodable<[ChatContent]>?
         
         if let string = codable as? String {
@@ -62,22 +65,24 @@ public struct Message: Codable, Equatable {
             stringOrCodable = nil
         }
         
-        self.init(role: role, content: stringOrCodable, name: name, functionCall: functionCall)
+        self.init(role: role, content: stringOrCodable, name: name, functionCall: functionCall, imageData: imageData)
     }
     
-    public init(role: Role, content: StringOrCodable<[ChatContent]>? = nil, name: String? = nil) {
+    public init(role: Role, content: StringOrCodable<[ChatContent]>? = nil, name: String? = nil, imageData: Data? = nil) {
         self.role = role
         self.content = content
         self.name = name
         self.functionCall = nil
+        self.imageData = imageData
 //        self.toolCalls = []
     }
     
-    public init(role: Role, content: StringOrCodable<[ChatContent]>? = nil, name: String? = nil, functionCall: ChatFunctionCall?) {
+    public init(role: Role, content: StringOrCodable<[ChatContent]>? = nil, name: String? = nil, functionCall: ChatFunctionCall?, imageData: Data? = nil) {
         self.role = role
         self.content = content
         self.name = name
         self.functionCall = functionCall
+        self.imageData = imageData
 //        self.toolCalls = []
     }
     
@@ -85,13 +90,15 @@ public struct Message: Codable, Equatable {
         role: Role,
         content: StringOrCodable<[ChatContent]>? = nil,
         name: String? = nil,
-        toolCalls: [ToolCall]?
+        toolCalls: [ToolCall]?,
+        imageData: Data? = nil
     ) {
         self.role = role
         self.content = content
         self.name = name
 //        self.toolCalls = toolCalls
         self.functionCall = nil
+        self.imageData = imageData
     }
 
     public func encode(to encoder: Encoder) throws {
@@ -102,9 +109,10 @@ public struct Message: Codable, Equatable {
             try container.encode(name, forKey: .name)
         }
 
-        if let functionCall = functionCall {
-            try container.encode(functionCall, forKey: .functionCall)
+        if let imageData = imageData {
+            try container.encode(imageData, forKey: .imageData)
         }
+
 //        
 //        if let toolCalls = toolCalls {
 //            try container.encode(toolCalls, forKey: .toolCalls)
@@ -117,3 +125,5 @@ public struct Message: Codable, Equatable {
         }
     }
 }
+
+extension Data: Codable {}
