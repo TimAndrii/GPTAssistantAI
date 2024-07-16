3fe3ff32e6b8ee2f1879e12f82ef7efc30c4b1a2
Andrii Tim <and.tim@icloud.com>
2024-07-17 00:04:43 +0300

    update 3.0 RunRetreiveResult

--- Sources/OpenAI/Public/Models/RunRetrieveResult.swift
+++ Sources/OpenAI/Public/Models/RunRetrieveResult.swift
@@ -10,7 +10,7 @@ import Foundation
 public struct RunRetreiveResult: Codable, Equatable {
 
     public let status: String
-    public let requiredAction: RequiredAction?
+    public let requiredAction: RequiredAction
 
     enum CodingKeys: String, CodingKey {
         case status
@@ -18,14 +18,25 @@ public struct RunRetreiveResult: Codable, Equatable {
     }
 }
 
+// MARK: - RequiredAction
 public struct RequiredAction: Codable, Equatable {
-    public let toolCalls: [ToolCallValue]?
+    public let submitToolOutputs: SubmitToolOutputs
+
+    enum CodingKeys: String, CodingKey {
+        case submitToolOutputs = "submit_tool_outputs"
+    }
+}
+
+// MARK: - SubmitToolOutputs
+public struct SubmitToolOutputs: Codable, Equatable {
+    public let toolCalls: [ToolCallValue]
 
     enum CodingKeys: String, CodingKey {
         case toolCalls = "tool_calls"
     }
 }
 
+// MARK: - ToolCall
 public struct ToolCallValue: Codable, Equatable {
     public let id: String
 }
