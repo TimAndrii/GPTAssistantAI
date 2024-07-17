a4c70ce36e9b4c767887e0534277ddca52b07724
Andrii Tim <and.tim@icloud.com>
2024-07-17 12:54:25 +0300

    update RunsQuery

--- Sources/OpenAI/Public/Models/RunsQuery.swift
+++ Sources/OpenAI/Public/Models/RunsQuery.swift
@@ -10,13 +10,16 @@ import Foundation
 public struct RunsQuery: Codable {
 
     public let assistantId: String
+    public let parallelToolCalls: Bool
 
     enum CodingKeys: String, CodingKey {
         case assistantId = "assistant_id"
+        case parallelToolCalls = "parallel_tool_calls"
     }
     
-    public init(assistantId: String) {
+    public init(assistantId: String, parallelToolCalls: Bool = true) {
 
         self.assistantId = assistantId
+        self.parallelToolCalls = parallelToolCalls
     }
 }
